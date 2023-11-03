0e46c60710558e01be64273687e514447f8ecd7b
kyrolosnissem <kyrolosnissem3@gmail.com>
2023-11-02 18:10:14 -0700

    c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,41 +1,27 @@
 #include "main.h"
 
-/**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
- */
-
-char *_memest(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
-
-	while (n--)
-		*s++ = b;
-
-	return (ptr);
-}
 
 /**
- * *_calloc - allocates memory for an array, using malloc
- * @nmemb: array length
- * @size: size of each element
- * Return: pointer
- */
-
+* _calloc - allocates memory for an array of @nmemb elements of
+* @size bytes each and returns a pointer to the allocated memory.
+*
+* @nmemb: allocate memory for array
+* @size: allocate element of size bytes
+*
+* Return: pointer to the allocated memory.
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
+	char *a;
+	unsigned int b;
 
-	if (size == 0 || nmemb == 0)
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(sizeof(int) * nmemb);
-
-	if (m == 0)
+	a = malloc(nmemb * size);
+	if (a == NULL)
 		return (NULL);
-	_memset(m, 0, sizeof(int) * nmemb);
-
-	return (m);
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
 }
