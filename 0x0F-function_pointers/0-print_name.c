0520c611f748089d411686b0913bd87cdaff7883
kyrolosnissem <kyrolosnissem3@gmail.com>
2023-11-08 13:37:01 -0800

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,12 @@
  * print_name - print a name
  * @name: string name
  * @f: pointer to function
- * Return: nothing
+ *
+ * Return: void
  **/
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name @@ f)
+	if (name && f)
 		f(name);
 }
