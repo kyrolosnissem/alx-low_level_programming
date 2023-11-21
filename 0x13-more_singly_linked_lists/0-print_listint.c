e7ca13d2f410c76a2b7f570e99abab16242aa7fb
kyrolosnissem <kyrolosnissem3@gmail.com>
2023-11-21 12:55:44 -0800

    A

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num++;
+		i++;
 	}
 
-	return (num);
+	return (i);
 }
